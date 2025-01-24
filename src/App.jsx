37351e8cac1b82ff9db8acefd0a11b1c8754b893
Mainul Hasan Khan <72866491+TechMainul-dev@users.noreply.github.com>
2025-01-25 00:48:28 +0600

    init tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css';
+import Banner from './Banner';
 
 function App() {
-  return <div className="container">This project start with react</div>;
+  return (
+    <>
+      <h1 className="m-2 text-3xl text-red-600 font-bold underline">
+        Hello world!
+      </h1>
+      <Banner></Banner>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Banner.jsx
@@ -0,0 +1,5 @@
+const Banner = () => {
+  return <div className="bg-gray-600 p-2 rounded-md-border">Banner</div>;
+};
+
+export default Banner;
