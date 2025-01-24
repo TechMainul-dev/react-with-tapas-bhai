7ad39b9a64447ed15920d7f1fa6befa8f2dbac63
Mainul Hasan Khan <72866491+TechMainul-dev@users.noreply.github.com>
2025-01-25 01:02:20 +0600

    task: 01

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import Banner from './Banner';
 function App() {
   return (
     <>
-      <h1 className="m-2 text-3xl text-red-600 font-bold underline">
-        Hello world!
-      </h1>
       <Banner></Banner>
     </>
   );
--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,5 +1,9 @@
 const Banner = () => {
-  return <div className="bg-gray-600 p-2 rounded-md-border">Banner</div>;
+  return (
+    <div className="text-xl text-green-600 bg-blue-950 p-4 mt-10 mx-auto w-max text-center">
+      We are here to learn React from tapaScript
+    </div>
+  );
 };
 
 export default Banner;
